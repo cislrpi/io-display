f5af2d0396c5dd6c61d92bf2d468cd7c172d9b73
Matthew Peveler <matt.peveler@gmail.com>
2019-05-24 11:11:58 -0400

    more work

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ class DisplayWorker {
   constructor(io) {
     this.io = io;
     this.displayContextFactory = new DisplayContextFactory(io);
+    this.displayContext;
+    this.uniformGridCellSize;
   }
 
   openDisplayWorker(name, display, contentGrid) {
@@ -19,9 +21,34 @@ class DisplayWorker {
       let displayWindow = displayContext.getDisplayWindowSync(name);
       await displayWindow.clearContents();
       let uniformGridCellSize = await displayWindow.getUniformGridCellSize();
+      this.uniformGridCellSize = uniformGridCellSize;
+      this.displayContext = displayContext;
       return new Promise((resolve, _) => {
         resolve({displayContext: displayContext, uniformGridCellSize: uniformGridCellSize});
       });
+    });
+  }
+
+  displayUrl(url, options) {
+    options = Object.assign(
+      {
+        position: {
+          'grid-left': 1,
+          'grid-top': 1
+        },
+        nodeintegration: false,
+        uiDraggable: true,
+        uiClosable: true
+      },
+      options
+    );
+
+    options.url = url;
+
+    options.width = (options.widthFactor * this.uniformGridCellSize.width) + 'px';
+    options.height = (options.heightFactor * this.uniformGridCellSize.height) + 'px';
+    this.displayContext.createViewObject(options, 'main').then(m => {
+      console.log(m);
     });
   }
 }
